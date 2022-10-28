3078edfd950883d8da5d558c9ece313436d28eb3
Aldrin Jenson <aldrinjenson@gmail.com>
2022-10-28 10:16:53 +0530

    initial

---
+++ sketch.js
@@ -0,0 +1,40 @@
+/// <reference path="lib/p5.global-mode.d.ts" />
+
+"use strict";
+
+let boxHeight, boxWidth
+function setup() {
+  angleMode(DEGREES);
+  createCanvas(400, 400, P2D);
+  background(200);
+  boxWidth = width / 3
+  boxHeight = height / 3
+}
+
+let grid = [
+  ['X', 'O', 'X'],
+  ['O', 'O', 'X'],
+  ['X', 'X', 'X'],
+]
+
+function draw() {
+  stroke('black')
+  strokeWeight(2)
+  line(0, boxHeight, width, boxHeight)
+  line(0, boxHeight * 2, width, boxHeight * 2)
+  line(boxWidth, 0, boxWidth, height)
+  line(boxWidth * 2, 0, boxWidth * 2, height)
+
+  strokeWeight(10)
+  textSize(40)
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      const el = grid[i][j]
+      let textX = (i * boxWidth) + boxWidth / 2
+      let textY = (j * boxHeight) + boxHeight / 2
+      // translate(-boxWidth / 4, -boxHeight / 4)
+      text(el, textX, textY);
+    }
+  }
+
+}
