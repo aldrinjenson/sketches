df85dae7256acab2e5220e53f54d5ae3455a7178
Aldrin Jenson <aldrinjenson@gmail.com>
2022-10-30 12:08:20 +0530

    update fractal

--- circle-recursion/sketch.js
+++ circle-recursion/sketch.js
@@ -2,27 +2,36 @@
 
 "use strict";
 
+let dxSlider;
+let radiusSlider;
+let halvingSlider;
+let dx;
 function setup() {
   createCanvas(400, 400, P2D);
   background(0);
   stroke(0, 255, 0);
   noFill();
+  dxSlider = createSlider(2, 4, 2, 0.05);
+  radiusSlider = createSlider(2, 1000, 200);
 }
 
 function draw() {
   translate(width / 2, height / 2);
-  drawCircle(0, 0, 500);
+  background(0);
+  drawCircle(0, 0, radiusSlider.value());
 }
 
-let dx = 123;
-const drawCircle = (x, y, radius) => {
-  ellipse(x, y, radius);
-  if (radius > 10) {
-    let newRadius = radius / 2;
-    drawCircle(x - dx, y, newRadius);
-    drawCircle(x + dx, y, newRadius);
-    // drawCircle(x - newRadius, y, newRadius);
-    drawCircle(x, y + dx, newRadius);
-    drawCircle(x, y - dx, newRadius);
+const drawCircle = (x, y, rad) => {
+  ellipse(x, y, rad);
+  let newRad = rad / dxSlider.value();
+  if (newRad > 2) {
+    stroke("green");
+    drawCircle(x + newRad, y, newRad);
+    stroke("red");
+    drawCircle(x - newRad, y, newRad);
+    stroke("yellow");
+    drawCircle(x, y + newRad, newRad);
+    // stroke("blue");
+    // drawCircle(x, y - newRad, newRad);
   }
 };
