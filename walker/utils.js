b44222bd48d5c6c60e7c4e3394b7f883b0b7a434
Aldrin Jenson <aldrinjenson@gmail.com>
2023-04-29 15:38:58 +0530

    add base files

--- walker/utils.js
+++ walker/utils.js
@@ -84,3 +84,70 @@ const checkTieGame = (grid) => {
   }
   return true;
 };
+
+const generateRandomPair = () => {
+  let i, j;
+  for (i = 0; i < grid.length; i++) {
+    for (j = 0; j < grid[0].length; j++) {
+      if (!grid[i][j]) return [i, j];
+    }
+  }
+};
+
+const updateStatus = () => {
+  let winner = null;
+  let winnerName = "";
+  if (checkPlayerWin(grid, human)) {
+    winnerName = "Human";
+    winner = human;
+    message = `Game Over!!\nPlayer ${winnerName} has won!`;
+  } else if (checkPlayerWin(grid, ai)) {
+    winnerName = "AI";
+    winner = ai;
+    message = `Game Over!!\nPlayer ${winnerName} has won!`;
+  } else if (checkTieGame(grid)) {
+    winner = "tie";
+    message =
+      "Game Over!!\nNo other possible moves available! Refresh the page to continue again";
+  }
+  currPlayer = players[playerIndex];
+  return winner;
+};
+
+const isMovesLeft = () => {
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (!grid.length) return true;
+    }
+  }
+  return false;
+};
+
+function evaluate(b) {
+  // Checking for Rows for X or O victory.
+  let player = "X",
+    opponent = "O";
+  console.log({ player, opponent });
+
+  for (let row = 0; row < 3; row++) {
+    if (b[row][0] == b[row][1] && b[row][1] == b[row][2]) {
+      if (b[row][0] == player) return +10;
+      else if (b[row][0] == opponent) return -10;
+    }
+  } // Checking for Columns for X or O victory.
+  for (let col = 0; col < 3; col++) {
+    if (b[0][col] == b[1][col] && b[1][col] == b[2][col]) {
+      if (b[0][col] == player) return +10;
+      else if (b[0][col] == opponent) return -10;
+    }
+  } // Checking for Diagonals for X or O victory.
+  if (b[0][0] == b[1][1] && b[1][1] == b[2][2]) {
+    if (b[0][0] == player) return +10;
+    else if (b[0][0] == opponent) return -10;
+  }
+  if (b[0][2] == b[1][1] && b[1][1] == b[2][0]) {
+    if (b[0][2] == player) return +10;
+    else if (b[0][2] == opponent) return -10;
+  } // Else if none of them have // won then return 0
+  return 0;
+}
